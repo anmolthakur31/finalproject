488636aaf11f23b0af8c4219d99c2a2a6ca2db19
hemant8998 <hemant7150@gmail.com>
2017-04-27 13:48:18 +0530

    static_pages

---
+++ app/controllers/extras_controller.rb
@@ -0,0 +1,12 @@
+class ExtrasController < ApplicationController
+	def faq
+	end
+	def tnc
+	end
+	def privacy_policy
+	end
+	def about_us
+	end
+	def our_team
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :services
+  get 'faq' => 'extras#faq'
+  get 'our_team' => 'extras#our_team'
+  get 'about_us' => 'extras#about_us'
+  get 'tnc' => 'extras#tnc'
+  get 'privacy_policy' => 'extras#privacy_policy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
