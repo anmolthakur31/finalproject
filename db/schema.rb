94573a0ccedc2ae6bb734b7efb9e6bd8b23b6165
anmolthakur31 <anmolthakur110@gmail.com>
2017-05-04 17:56:43 +0530

    userroles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504072935) do
+ActiveRecord::Schema.define(version: 20170504115628) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 20170504072935) do
     t.datetime "photo_updated_at"
   end
 
+  create_table "userroles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "rolename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "phone"
@@ -72,8 +78,10 @@ ActiveRecord::Schema.define(version: 20170504072935) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "userrole_id",            default: 3
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["userrole_id"], name: "index_users_on_userrole_id", using: :btree
   end
 
   create_table "uservehicles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -106,6 +114,7 @@ ActiveRecord::Schema.define(version: 20170504072935) do
   end
 
   add_foreign_key "models", "brands"
+  add_foreign_key "users", "userroles"
   add_foreign_key "uservehicles", "users"
   add_foreign_key "uservehicles", "vehicles"
   add_foreign_key "vehicles", "brands"
