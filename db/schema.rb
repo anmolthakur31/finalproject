8e152e2578819c2dfe3d4537b933a96f95f7d61a
gauri1995 <gauriohri14@gmail.com>
2017-05-21 20:23:21 +0530

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520122528) do
-
+ActiveRecord::Schema.define(version: 20170520131416) do
 
   create_table "booked_services", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "booking_id"
